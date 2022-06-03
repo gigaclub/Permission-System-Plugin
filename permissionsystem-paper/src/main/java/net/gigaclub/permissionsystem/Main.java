804b7ac6b91196c81cae03b6db24aae60e674171
Kevin <xxkevtvxx@gmail.com>
2022-06-03 06:31:10 +0200

    setup

--- permissionsystem-paper/src/main/java/net/gigaclub/permissionsystem/Main.java
+++ permissionsystem-paper/src/main/java/net/gigaclub/permissionsystem/Main.java
@@ -1,12 +1,13 @@
-package net.gigaclub.pluginname;
+package net.gigaclub.permissionsystem;
 
-import net.md_5.bungee.api.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends Plugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+
     }
 
     @Override
