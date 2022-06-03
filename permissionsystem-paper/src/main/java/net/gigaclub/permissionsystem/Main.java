c8060a451c506c086fdac8ba0e98a01786fd8448
Kevin <xxkevtvxx@gmail.com>
2022-06-03 21:40:33 +0200

    wip

--- permissionsystem-paper/src/main/java/net/gigaclub/permissionsystem/Main.java
+++ permissionsystem-paper/src/main/java/net/gigaclub/permissionsystem/Main.java
@@ -1,17 +1,47 @@
 package net.gigaclub.permissionsystem;
 
 import de.dytanic.cloudnet.driver.permission.IPermissionManagement;
+import net.gigaclub.permissionsystemapi.PermissionSystem;
+import net.gigaclub.translation.Translation;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.dytanic.cloudnet.driver.CloudNetDriver;
+import org.json.JSONArray;
+
+import java.io.File;
 
 public final class Main extends JavaPlugin {
 
     private static Main plugin;
+    private static Translation translation;
+    private static PermissionSystem permissionSystem;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         setPlugin(this);
+
+        File file = new File("plugins//" + "Odoo", "config.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        setTranslation(new Translation(
+                config.getString("Odoo.Host"),
+                config.getString("Odoo.Database"),
+                config.getString("Odoo.Username"),
+                config.getString("Odoo.Password")
+        ));
+        translation.setCategory("translation");
+
+        setPermissionSystem(new PermissionSystem(
+                config.getString("Odoo.Host"),
+                config.getString("Odoo.Database"),
+                config.getString("Odoo.Username"),
+                config.getString("Odoo.Password")
+        ));
+
+        JSONArray groups = Main.getPermissionSystem().getAllGroups();
 
     }
 
@@ -26,6 +56,22 @@ public final class Main extends JavaPlugin {
 
     public static void setPlugin(Main plugin) {
         Main.plugin = plugin;
+    }
+
+    public static Translation getTranslation() {
+        return Main.translation;
+    }
+
+    public static void setTranslation(Translation translation) {
+        Main.translation = translation;
+    }
+
+    public static PermissionSystem getPermissionSystem() {
+        return Main.permissionSystem;
+    }
+
+    public static void setPermissionSystem(PermissionSystem permissionSystem) {
+        Main.permissionSystem = permissionSystem;
     }
 
 }
